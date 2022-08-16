456e082c43488aa578823fdf1ba6af52831adb25
EliavYair1 <77952937+EliavYair1@users.noreply.github.com>
2022-08-17 02:43:09 +0300

    last commit 2:45

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,46 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const { src, dest, watch, series } = require('gulp');
+const prefixer = require('gulp-autoprefixer');
+const terser = require('gulp-terser');
+const minify = require('gulp-clean-css');
+const webp = require('gulp-webp');
+const imagemin = import('gulp-imagemin');
+const mozjpeg = import('imagemin-mozjpeg');
+const pngquant = import('imagemin-pngquant');
+const optipng = import('imagemin-optipng');
 const reload = browserSync.reload;
+
 function style() {
-  return gulp
-    .src('./scss/*.scss')
+  return src('./scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+    .pipe(prefixer())
+    .pipe(minify())
+    .pipe(dest('./dist/css'))
     .pipe(browserSync.stream());
+}
+// js files
+function jsmin() {
+  return src('js/*.js').pipe(terser()).pipe(dest('./dist/js'));
+}
+
+// image loading
+async function imgOp() {
+  return await src('./images/**/*.{jpg,png}')
+    .pipe(
+      imagemin([
+        // pngquant({ quality: [0.5, 0.5] }),
+        mozjpeg({ quality: 80, progressive: true }),
+        optipng({ optimizationLevel: 5 }),
+      ])
+    )
+    .pipe(dest('./dist/images'));
+}
+
+function webpImg() {
+  return src('dist/images/*.{jpg,png}')
+    .pipe(webp())
+    .pipe(dest('./dist/images'));
 }
 
 function watching() {
@@ -17,9 +50,10 @@ function watching() {
       baseDir: './',
     },
   });
-  gulp.watch('./scss/*.scss');
-  gulp.watch('*.html').on('change', reload);
+  watch('./scss/*.scss', style);
+  watch('./images/**/*.{jpg,png}', imgOp);
+  watch('./images/**/*.{jpg,png}', webpImg);
+  watch('*.html').on('change', reload);
 }
-exports.watching = watching;
-exports.style = style;
-exports.build = series(style, watch);
+
+exports.default = series(style, jsmin, webpImg, watching);
